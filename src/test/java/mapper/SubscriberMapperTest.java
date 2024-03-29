db1813b0263c36c16cafa1a4411fd5988242d536
Ahmadshoh <ahmadshohraupov@gmail.ru>
2024-03-29 15:16:37 +0500

    final changes

--- src/test/java/mapper/SubscriberMapperTest.java
+++ src/test/java/mapper/SubscriberMapperTest.java
@@ -26,7 +26,7 @@ public class SubscriberMapperTest extends TestCase {
 
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
             subscriberMapper = sqlSessionFactory.openSession().getMapper(SubscriberMapper.class);
-            var s = new Subscriber().setName("firstName").setId(3L);
+            var s = new Subscriber().setName("firstName").setId(1L);
             var tarif=new Tariff();
             tarif.setId(1L);
 
