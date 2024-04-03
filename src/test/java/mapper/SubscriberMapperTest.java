e31bb0e9d22ee18720d7ad9e6194cc12990e262e
Ahmadshoh <ahmadshohraupov@gmail.ru>
2024-04-03 17:24:45 +0500

    final changes

--- src/test/java/mapper/SubscriberMapperTest.java
+++ src/test/java/mapper/SubscriberMapperTest.java
@@ -2,51 +2,50 @@ package mapper;
 
 import dal.Subscriber;
 import dal.Tariff;
-import junit.framework.TestCase;
- import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.*;
- import org.testng.annotations.AfterTest;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import java.io.IOException;
 import java.io.Reader;
 
 
-public class SubscriberMapperTest extends TestCase {
+public class SubscriberMapperTest {
 
     SubscriberMapper subscriberMapper;
     SqlSessionFactory sqlSessionFactory;
-     Reader reader = null;
-@BeforeTest
-public void connectDB(){
-    try {
+    Reader reader = null;
 
-        reader = Resources
-            .getResourceAsReader("mybatis-config.xml");
+    @BeforeTest
+    public void connectDB() {
+        try {
 
-    sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-     subscriberMapper = sqlSessionFactory.openSession().getMapper(SubscriberMapper.class);
-     } catch (IOException e) {
-         throw new RuntimeException(e);
+            reader = Resources
+                    .getResourceAsReader("mybatis-config.xml");
+
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            subscriberMapper = sqlSessionFactory.openSession().getMapper(SubscriberMapper.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-}
 
-@AfterTest
-public void clear(){
-    subscriberMapper.delete(2);
+    @AfterTest
+    public void clear() {
+        subscriberMapper.delete(2);
+    }
 
-
-}
     @Test
     public void SubscriberTest() {
 
+        var s = new Subscriber().setName("firstName").setId(2L);
+        var tarif = new Tariff();
+        tarif.setId(2L);
 
-            var s = new Subscriber().setName("firstName").setId(2L);
-            var tarif=new Tariff();
-            tarif.setId(2L);
-
-            s.setTariff(tarif);
-            subscriberMapper.insert(s);
+        s.setTariff(tarif);
+        subscriberMapper.insert(s);
 
     }
 
